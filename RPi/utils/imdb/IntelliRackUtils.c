21f92eddf7fe1ee6798b6ed83e7b7889e85fa330
Florian Wernli <0pq76r@gmail.com>
2013-01-23 09:34:18 +0100

    asdf

--- RPi/utils/imdb/IntelliRackUtils.c
+++ RPi/utils/imdb/IntelliRackUtils.c
@@ -2,31 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
-char title[56]={0};
-char *dvd_device="/dev/dvd";
+void DVDName(){
+	FILE *filehandle = NULL;
+	char title[56]={0};
+	char *dvd_device="/dev/dvd";
 
-int main(){
-	FILE *filehandle = 0;
 	int  i;
 
 	if (! (filehandle = fopen(dvd_device, "r"))) {
 		fprintf(stderr, "Couldn't open %s for title\n", dvd_device);
 		strcpy(title, "unknown");
-		return -1;
 	}
 
 	if ( fseek(filehandle, 32808, SEEK_SET )) {
 		fclose(filehandle);
 		fprintf(stderr, "Couldn't seek in %s for title\n", dvd_device);
 		strcpy(title, "unknown");
-		return -1;
 	}
 
 	if ( 32 != (i = fread(title, 1, 32, filehandle)) ) {
 		fclose(filehandle);
 		fprintf(stderr, "Couldn't read enough bytes for title.\n");
 		strcpy(title, "unknown");
-		return -1;
 	}
 
 	fclose (filehandle);
@@ -36,6 +33,4 @@ int main(){
 	if(title[i] == ' ') title[i] = '\0';
 
 	printf("%s", title);
-
-	return 0;
 }
---
+++ RPi/utils/imdb/IntelliRackUtils.pm
@@ -0,0 +1,23 @@
+package IntelliRackUtils;
+
+use strict;
+use warnings;
+use Data::Dumper;
+use IMDBparser;
+use warnings;
+
+require Exporter;
+require DynaLoader;
+
+our @ISA = qw(Exporter DynaLoader);
+our @EXPORT = qw(
+
+);
+
+our $VERSION = '0.01'
+
+bootstrap IntelliRackUtils $Version
+
+1;
+
+__END__
